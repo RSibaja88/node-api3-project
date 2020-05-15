ed1807d70b634ca9562279856b742ff88cb54f3b
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-05-14 22:49:02 -0400

    mvp

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,78 @@
 const express = require('express');
 
 const router = express.Router();
+const pDb = require("./postDb");
 
 router.get('/', (req, res) => {
-  // do your magic!
+  pDb.get()
+  .then(post => {
+    res.status(200).json(post)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({ errorMessage: "Trouble acessing the posts"})
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+ const id = req.params.id;
+  pDb.getById(id)
+  .then(post => {
+    res.status(200).json({post})
+  })
+  .catch(err => {
+    res.status(500).json({errorMessage: "Could not retrieve specified ID"})
+  })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostById, (req, res) => {
+  const id = req.params.id
+  pDb.remove(id)
+    .then(post => {
+      res.status(200).json({post})
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message : "The post could not be removed."})
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostById, validatePost, (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+    pDb.update(id, data)
+      .then( post => {
+        res.status(201).json({post})
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "Post was not updated"})
+      })
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+function validatePost(req, res, next) {
+  const data = req.body;
+  if(!data){
+    res.status(400).json({ message: 'missing post data.'})
+} else if(!data.text){
+    res.status(400).json({ message: "missing  required text field"})
+} else { 
+  next();
+  }
+}
+
+function validatePostById(req, res, next) {
+  const id = req.params.id;
+  pDb.getById(id)
+  .then(post => {
+    if(!post) {
+      res.status(404).json({error: 'The specified ID does not exist.'})
+    } else {
+      next();
+    }
+  })
 }
 
 module.exports = router;
